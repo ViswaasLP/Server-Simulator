255f603d144f7c4ba15873c563ffb8080c8ba8c4
viswaas98 <viswaas98@gmail.com>
2019-06-02 23:32:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ double negexdistime(double rate){
 int main(int argc, char *argv[])
 {
 	double lambda = atoi(argv[1]);
+	int numhosts = atoi(argv[2]);
+	Host host[numhosts];
 	int numframes = 0;
 	Queue * globalframeq = new Queue;
 	while(globalframeq->getSize()!=0){
